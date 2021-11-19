54091cde40c49d8c7bb91086eb7b3a5c9c2b4b47
Nhi Dinh <tuyetnhi1607@github.com>
2021-11-19 16:22:12 +0700

    90%

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -2,37 +2,21 @@ import React, { useRef, useEffect } from "react";
 import "./book.scss";
 import avatar from "../../assets/nhi.png";
 import "boxicons";
-import {TagCloud} from "react-tagcloud";
-
-const data = [
-    { value: 'MongoDB', count: 18 },
-    { value: 'JavaScript', count: 38 },
-    { value: 'React', count: 30 },
-    { value: 'HTML5', count: 10 },
-    { value: 'CSS3', count: 30 },
-    { value: 'MongoDB', count: 22 },
-    { value: 'Ant Design', count: 7 },
-    { value: 'Material-UI', count: 25 },
-    { value: 'Github', count: 32 },
-    { value: 'Linux', count: 17 },
-    { value: 'MongoDB', count: 10},
-    { value: 'JavaScript', count: 18 },
-    { value: 'React', count: 40 },
-    { value: 'HTML5', count: 30 },
-    { value: 'CSS3', count: 14 },
-    { value: 'MongoDB', count: 40 },
-    { value: 'Ant Design', count: 20 },
-    { value: 'Material-UI', count: 12 },
-    { value: 'Github', count: 7 },
-    { value: 'Linux', count:30 },
-]
+import video1 from "../../assets/video1.mp4"
+import video2 from "../../assets/video2.mp4"
 
 function Book() {
   useEffect(() => {
     let turnPageBtn = document.querySelectorAll(".book-page-item");
     turnPageBtn.forEach((el, index) => {
-      console.log("el", el);
+      el.style.zIndex = 20 - index;
+      console.log("el", el, index);
       el.onclick = (e) => {
+        let videos = document.querySelectorAll('video')
+
+        videos.forEach((el_1) => {
+            el_1.pause()
+        })
         if (el.classList.contains("turn")) {
           el.classList.remove("turn");
           setTimeout(() => {
@@ -40,11 +24,27 @@ function Book() {
           }, 600);
         } else {
           el.classList.add("turn");
-          el.style.zIndex = 200 - index;
+          el.style.zIndex = 20 - index;
         }
       };
     });
-  }, []);
+    let videos = document.querySelectorAll('video')
+
+    videos.forEach((el) => {
+        el.onplay = () => {
+            videos.forEach((el_1) => {
+                if (el === el_1) {
+                    el.play()
+                } else {
+                    el_1.pause()
+                }
+            })
+        }
+    })
+    
+    //open book
+
+  },[]);
 
   return (
     <div className="book">
@@ -110,45 +110,197 @@ function Book() {
               <br />
               <p> Have basic knowledge of front end:</p>
               <h3>
-                HTML/CSS, <br />
-                JavaScript, <br />
+                HTML,
+                CSS, 
+                JavaScript, 
                 ReactJS.
               </h3>
               <br />
               <p> Worked with:</p>
               <h3>
-                Ant-Design, <br />
+                Ant-Design, 
                 Material-UI,
-                <br /> MongoDB, <br />
-                Github, <br />
+               MongoDB, 
+                Github, 
                 Linux.
               </h3>
-              <div className="cloud">
-                <TagCloud
-                    minSize={12}
-                    maxSize={40}
-                    tags={data}
-                    className="simple-cloud"
-                />
+              <br /> <br/>
+              <h1>Experiences</h1>
+              <br />
+              <p>Worked as a trainee</p>
+              <h3>
+                AI.SOFT
+                <span class="time">10/2018 - 4/2020</span>
+              </h3>
+              <br />
+              <br />
+              <p>Worked as a trainee</p>
+              <h3>
+                VNPT Technology
+                <span class="time">07/2021 - 08/2021</span>
+              </h3>
+              <br />
+              <br />
             </div>
-            </div>
-            
           </div>
         </div>
         <div className="book-page-item turn" id="2">
           <div className="book-page-item-font">
-                
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video2} type="video/mp4" />
+              </video>
+              <h1>Food Order Website 1</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
           </div>
           <div className="book-page-item-back">
-            <h1>Back2</h1>
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video1} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+              <h1>Food Order Website2</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
           </div>
         </div>
         <div className="book-page-item turn" id="3">
           <div className="book-page-item-font">
-            <h1>Font3</h1>
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video2} type="video/mp4" />
+              </video>
+              <h1>Food Order Website3</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
           </div>
           <div className="book-page-item-back">
-            <h1>Back3</h1>
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video1} type="video/mp4" />
+              </video>
+              <h1>Food Order Website4</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  cursor="alias"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-page-item turn" id="4">
+          <div className="book-page-item-font">
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video2} type="video/mp4" />
+              </video>
+              <h1>Food Order Website5</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="book-page-item-back">
+            <div className="demo-wrapper">
+              <video width="100%" height="auto" controls>
+                <source src={video1} type="video/mp4" />
+              </video>
+              <h1>Food Order Website6</h1>
+              <h2>
+                This is a simple project I write in HTML, CSS, React JS. This
+                project helps me practice layout, animation, and especially
+                Responsive.
+              </h2>
+              <div>
+                <i class="bx bx-link"></i>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://blooming-woodland-27991.herokuapp.com/"
+                >
+                  https://blooming-woodland-27991.herokuapp.com/
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="book-page-last">
+          <div className="contacts-wrapper">
+            <h1>Contact me</h1>
+            <p>Get in touch, if you have any work or questions.</p>
+            <div className="form-group">
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Email" />
+              <input type="text" placeholder="Subject" />
+              <textarea placeholder="Message" />
+            </div>
+            <button>Submit</button>
           </div>
         </div>
       </div>
